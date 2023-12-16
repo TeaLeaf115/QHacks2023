081efbc0dd6b5d2b0299e66b463dde6de349c1b5
Kevin Zhang <kevin@Kevins-MacBook-Pro.local>
2023-12-16 11:30:35 -0500

    Colors

--- src/App.py
+++ src/App.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class App:
     def __init__(self):
         pygame.init()
@@ -14,18 +15,23 @@ class App:
 
         self.screen = pygame.display.set_mode(
             self.resolution,
-            pygame.DOUBLEBUF
+            pygame.DOUBLEBUF | pygame.FULLSCREEN
         )
-        
+
         self.clock = pygame.time.Clock()
         self.runtime = True
-        
 
     def run(self):
 
         while self.runtime:
-            for event in pygame.events.get():
-                pass
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.runtime = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.runtime = False
 
             self.draw()
             self.update()
@@ -44,6 +50,7 @@ class App:
 
     def update(self):
         pass
+
 
 if __name__ == '__main__':
     App().run()
--- src/sprite.py
+++ src/sprite.py
@@ -1,4 +1,20 @@
 import pygame
 
-class Sprite:
-    pass
+class Sprite(pygame.sprite.Sprite):
+    def __init_(self, coords: tuple, size: tuple, groups: pygame.sprite.Group, game):
+        super().__init__()
+        self.game = game
+
+        self.coords = pygame.math.Vector2(*coords)
+        self.size = pygame.math.Vector2(*size)
+
+        # images and rects
+        self.image = pygame.Surface(size)
+        self.rect = self.image.getRect(topleft=coords)
+
+        # render
+        self.sprite_layer = 0
+
+        # animation
+        self.frame = 0
+    
