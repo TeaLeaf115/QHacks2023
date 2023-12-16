ba1b5122d26f881afb94c3cc418b64140f653225
Kevin Zhang <kevin@Kevins-MacBook-Pro.local>
2023-12-16 13:05:21 -0500

    Player

--- src/entity.py
+++ src/entity.py
@@ -7,11 +7,16 @@ class Entity(Sprite):
     def __init__(self, coords: tuple, size: tuple, game, groups):
         super().__init__(coords, size, game, groups)
 
+        self.velocity = pygame.math.Vector2()
+        self.acceleration = pygame.math.Vector2()
+
     def movement(self):
-        pass
+        self.velocity += self.acceleration * 5
 
     def collision(self):
         pass
 
     def update(self):
-        pass
+        self.movement()
+
+    
---
+++ src/player.py
@@ -0,0 +1,29 @@
+from entity import Entity
+
+import pygame
+
+
+class Player(Entity):
+    def __init__(self, coords: tuple, size: tuple, game, groups):
+        super().__init__(coords, size, game, groups)
+
+        self.velocity = pygame.math.Vector2()
+        self.acceleration = pygame.math.Vector2()
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        left = keys[pygame.K_a]
+        right = keys[pygame.K_d]
+        down = keys[pygame.K_s]
+        up = keys[pygame.K_w]
+        
+        self.acceleration.xy = right - left, down - up
+
+        self.velocity += self.acceleration
+        self.coords + self.velocity
+
+    def collision(self):
+        pass
+
+    def update(self):
+        self.movement()
